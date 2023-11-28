6353f042e8716791983e56a42359c319dc1694d2
Kaosarat Aina <kaosarataina@Kaosarats-MacBook-Air.local>
2023-11-28 15:31:36 -0500

    move file

--- train.py
+++ train.py
@@ -4,7 +4,7 @@ s = sys.stdin.readlines()
 
 total_number_of_tokens = 0
 
-tag_frequency = {}
+word_total_count = {}
 word_tag = {}
 
 fd = open('wiki.conllu', 'r')
@@ -15,13 +15,18 @@ for line in fd.readlines():
 	if '\t' not in line:
 		continue
 	row = line.split('\t')
-	form = row[1]
+	form = row[1].strip()
 	tag = row[3]
 	if form not in word_tag:
 		word_tag[form]= {}
 	if tag not in word_tag[form]:
 		word_tag[form][tag] = 0
 	word_tag[form][tag] += 1
+	total_number_of_tokens += 1
+	
+	if form not in word_total_count:
+		word_total_count[form] = 0
+	word_total_count[form] += 1
 
 #for w in word_tag:
 	#print('%d\t%s' % (word_tag[tag], w))
@@ -38,3 +43,8 @@ for form in word_tag:
 		print('%d\t%s\t' % (word_tag[form][tag], tag), end='')
 	print('')
 
+print("# P\tcount\ttag\tform")
+for form in word_tag:
+    for tag, count in word_tag[form].items():
+        freq = count / word_total_count[form]
+        print(f"{freq:.2f}\t{count}\t{tag}\t{form}")
